e9fe0cdb588e43d7210c9005db0d1c7f644ba937
Eren Alp Aras <erenalparas@gmail.com>
2022-01-08 22:30:46 +0300

    commit

--- src/test/java/starter/n11/navigation/tasks/NavigateTo.java
+++ src/test/java/starter/n11/navigation/tasks/NavigateTo.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
 import starter.n11.navigation.user_interface.n11HomePage;
 
-public class NavigateTo {
+public class  NavigateTo {
     public static Performable theN11HomePage() {
         return Task.where("{0} opens the n11 home page",
                 Open.browserOn().the(n11HomePage.class));
--- src/test/java/starter/n11/navigation/user_interface/n11LoginPageElements.java
+++ src/test/java/starter/n11/navigation/user_interface/n11LoginPageElements.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.By;
 
 public class n11LoginPageElements {
 
-    public static Target FACEBOOK_BTN = Target.the("facebook btn").located(By.xpath("//body/div[@id='wrapper']/div[@id='contentWrapper']/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]"));
+    public static Target FACEBOOK_BTN = Target.the("facebook btn").located(By.cssSelector("div[class='facebook_large medium facebookBtn  btnLogin']"));
 
     public static Target FACEBOOK_EMAIL = Target.the("facebook email").located(By.cssSelector("div>input[type='text']"));
-    public static Target FACEBOOK_PASSWORD = Target.the("facebook password").located(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[2]/div[1]/input[1]"));
-    public static Target FACEBOOK_LOGIN_BTN = Target.the("facebook login btn").located(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[3]/label[2]/input[1]"));
-
+    public static Target FACEBOOK_PASSWORD = Target.the("facebook password").located(By.cssSelector("input[type='password']"));
+    public static Target FACEBOOK_LOGIN_BTN = Target.the("facebook login btn").located(By.cssSelector("input[name='login']"));
 
 }
